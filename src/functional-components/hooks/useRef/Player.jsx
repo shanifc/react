7cb7688b2e58fdcaa87939078f47ddf50c9aed20
shanifc <mhdshanif43@gmail.com>
2025-11-18 10:47:26 +0530

    commit

---
+++ src/functional-components/hooks/useRef/Player.jsx
@@ -0,0 +1,25 @@
+import { useState, useRef } from "react";
+
+export default function Player() {
+    var playerRef = useRef();
+
+    const [player, setPlayer] = useState("");
+
+
+    function handleButtonClick() {
+        setPlayer(playerRef.current.value)
+        playerRef.current.value = ""
+
+    }
+
+
+    return (
+        <section id="player">
+            <h2>Welcome to {player ?? "User"}</h2>
+            <p>
+                <input ref={playerRef} type="text" />
+                <button onClick={handleButtonClick}>Set Name</button>
+            </p>
+        </section>
+    );
+}
